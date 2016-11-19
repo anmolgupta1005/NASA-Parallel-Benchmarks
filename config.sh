a9e2e04a685f2fb61c97e66acefb7a4d2d5735a7
anmolgupta1005 <anmol.gupta1005@gmail.com>
2016-11-19 11:54:26 -0500

    Autmated the linking of config files

--- config.sh
+++ config.sh
@@ -1,3 +1,27 @@
-#!/bin/bash
+#!/bin/sh
 
 # Will place and link all the config files in the respective directories for installation
+
+NAS_PATH=`pwd`
+
+if [ -f ../config/make.def ]; 
+then
+	rm -f ../config/make.def
+fi
+
+if [ -f ../config/suite.def ]; 
+then
+	rm -f ../config/suite.def	
+fi
+
+if [ -f ../MPI_dummy/Makefile ]; 
+then
+	rm -f ../MPI_dummy/Makefile
+fi
+
+# Creating the files and linking to these files
+
+ln -s $NAS_PATH/config/make.def ../config/make.def
+ln -s $NAS_PATH/config/suite.def ../config/suite.def
+ln -s $NAS_PATH/MPI_dummy/Makefile ../MPI_dummy/Makefile
+
